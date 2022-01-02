8113782823eb6934ed8ec84fd2b986d129f1acb3
Elvis Chege <elvischege@icloud.com>
2022-01-02 05:15:17 +0200

    Added Credit based system

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+from requests.sessions import requote_uri
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from pymongo import MongoClient
@@ -38,5 +39,44 @@ class Register(Resource):
         })
 
         return jsonify({"Message": "Registration successful"})
+
+
+class Classify(Resource):
+    def post(self):
+        posted_data = request.get_json()
+
+        username = posted_data["username"]
+        password = posted_data["password"]
+        url = posted_data["url"]
+
+        #TODO
+        ret, err = auth_user(username, password)
+        if err:
+            return jsonify(ret)
+
+        credits = users.find({
+            "Username": username,
+        })[0]["Credits"]
+
+        if credits<=0:
+            return jsonify(gen_ret_dict(303, "Insuffiecient Credits!"))
+        
+        r = requests.get(url)
+        ret = {}
+
+        with open("temp.jpg", "wb") as f:
+            f.write(r.content)
+            proc = subprocess.Popen("python3 classify_image.py --model_dir=. --image_file=./temp.jpg")
+            proc.communicate()[0]
+            proc.wait()
+            with open("text.txt") as g:
+                ret = json.load(g)
+        
+        users.update_one({"Username": username}, 
+            {"$set": {
+                "Credits":credits-1
+            }})
+        return ret
+        
 
 app.run(host='0.0.0.0', port="3000")
